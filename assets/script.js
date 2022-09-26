fd972675c03d5c80ffebf7d7042371d7783c203c
dberry38 <108836058+dberry38@users.noreply.github.com>
2022-09-26 14:15:09 -0700

    GREAT SUCCESS   OpenWeather API is returning data according to user's city input

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,49 @@
-userSearch = $("#cityname")
-searchButton = $(".btn")
-recentList = $("#recent-list")
-resultsFor = $("#results-for")
-currentConditions = $("#current-conditions")
-fiveDay = $("#5-day")
+const userSearch = document.querySelector('#cityname')
+const searchButton = document.querySelector('#user-form')
+const recentList = $("#recent-list")
+const resultsFor = $("#results-for")
+const currentConditions = $("#current-conditions")
+const fiveDay = $("#5-day")
+const apiKey = "f22e02f6379f4d799e50d6593eecd302"
+// const openWeatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    var cityName = userSearch.value;
+
+    if (cityName) {
+        newQuery(cityName);
+
+        userSearch.value = '';
+    } else {
+        alert("I can't believe you've done this");
+    }
+};
+
+
+
+var newQuery = function (cityName) {
+    apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    displayWeather(data, cityName);
+                });
+            } else {
+                alert("I can't believe you've done this.");
+            }
+        });
+
+        };
+
+
+
+
+
+
+searchButton.addEventListener("submit", formSubmitHandler);
