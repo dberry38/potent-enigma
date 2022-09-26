04cf1ab57c1dc5cad25644b32974699447f59847
dberry38 <108836058+dberry38@users.noreply.github.com>
2022-09-26 14:21:05 -0700

    GREAT SUCCESS         Adding JS to enable searching by city name; openweather is responding correctly.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 const userSearch = document.querySelector('#cityname')
 const searchButton = document.querySelector('#user-form')
-const recentList = $("#recent-list")
-const resultsFor = $("#results-for")
-const currentConditions = $("#current-conditions")
-const fiveDay = $("#5-day")
+const recentList = document.querySelector("#recent-list")
+const resultsFor = document.querySelector("#results-for")
+const currentConditions = document.querySelector("#current-conditions")
+const fiveDay = document.querySelector("#five-day")
 const apiKey = "f22e02f6379f4d799e50d6593eecd302"
 // const openWeatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
